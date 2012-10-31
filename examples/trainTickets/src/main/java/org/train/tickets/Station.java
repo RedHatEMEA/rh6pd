b559fe6a7ddc0bf431b2775119ff698444fb3dfd
jread <jread@redhat.com>
2012-10-31 17:45:23 +0000

    Imported the work last minute Demo "trainTickets" project.

---
+++ examples/trainTickets/src/main/java/org/train/tickets/Station.java
@@ -0,0 +1,13 @@
+package org.train.tickets;
+
+public class Station {
+	public String name; 
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	} 
+}
