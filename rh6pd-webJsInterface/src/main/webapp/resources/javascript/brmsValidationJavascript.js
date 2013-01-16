bac174fca9efdeb342b3172bdde6a144f1b3cfe3
derekh99 <dhowlett@redhat.com>
2013-01-16 14:43:13 +0000

    Another big cleanup of the webJsInterface project.
    FIX: Poor validation on jBPM Human Tasks (Driver + Car) by externalising
    the javascript assocaited with both pages.
    
    REFACTOR: General cleanup of HTML and the splash page, so the styles look
    very similar now. Plenty of work to still do, but its getting better.
    
    FIX: The interface is now available at /rh6pd, instead of /rh6pd-jsWebInterface
    
    FIX: Cleaned up a whole load of URLs - removed all absolute URLs and changed
    them to relative (even though some URLs are relative to the root of the webserver)
    
    URLs are less likely to change now that we have the nice /rh6pd URL mapping.
    
    REFACTOR: Moved a bunch of images and the the javascript into
    resources/.../{images,javascript}

---
+++ rh6pd-webJsInterface/src/main/webapp/resources/javascript/brmsValidationJavascript.js
@@ -0,0 +1,144 @@
+function notEmpty(elem){
+	if (elem == null) {
+		return false;
+	}
+	
+	if(elem.value.length == 0){
+		return false;
+	}
+	return true;
+} 
+  
+function isNumeric(elem){
+	var numericExpression = /^[0-9]+$/;
+	if(elem.value.match(numericExpression)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isAlphabet(elem){
+	var alphaExp = /^[a-zA-Z0-9\_ .-@]+$/;
+	if(elem.value.match(alphaExp)){
+		return true;
+	} else { 
+		return false;
+	}
+}
+
+function isAlphanumeric(elem){
+	var alphaExp = /^[a-zA-Z0-9\_ .-@]+$/;
+	if(elem.value.match(alphaExp)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isFloat(elem){
+	if(elem.value.indexOf(".") < 0){
+ 		return false;
+	} else {
+  		if(parseFloat(elem.value)) {
+          return true;
+      	} else {
+          return false;
+      	}
+	}
+}
+
+function taskFormValidatorDriver() {
+	var i=0;
+	var myInputs = new Array();
+
+	myInputs[i] = document.getElementById("Name");
+	i++;
+
+	myInputs[i] = document.getElementById("Street");
+	i++;
+
+	myInputs[i] = document.getElementById("City");
+	i++;
+	
+	myInputs[i] = document.getElementById("Age");
+	i++;
+			
+	
+	var j=0;
+
+	if(notEmpty(myInputs[j]) && !isAlphanumeric(myInputs[j])) {
+		alert("Please enter valid Name");
+		myInputs[j].focus();
+		return false;
+	}	
+	j++;
+
+	if(notEmpty(myInputs[j]) && !isAlphanumeric(myInputs[j])) {
+		alert("Please enter valid Street");
+		myInputs[j].focus();
+		return false;
+	}	 
+	j++;
+
+	if(notEmpty(myInputs[j]) && !isAlphanumeric(myInputs[j])) {
+		alert("Please enter valid City");
+		myInputs[j].focus();
+		return false;
+	}	 
+	j++;
+	   
+	if(notEmpty(myInputs[j]) && !isAlphanumeric(myInputs[j])) {
+		alert("Please enter valid Age");
+		myInputs[j].focus();  
+		return false;
+	}	
+	j++;
+			
+	return true;
+}
+
+function taskFormValidatorCar() {
+	try {
+		validateField('Make', false, true, true);
+		validateField('Model', false, true, true);
+		validateField('Reg', false, true, true);	
+		
+		return true;      
+	} catch (e) {
+		return false;
+	}
+} 
+
+function highlightField(field, isStatusGood) {
+	console.log(field, isStatusGood);  
+	if (isStatusGood) {  
+		field.css('border', '2px solid green');
+	} else {
+		field.css('border', '2px solid red');
+	} 
+}
+
+function validateField(fieldId, acceptsOnlyNumbers, acceptsLetters, acceptsSpaces) {
+	var field = $('#' + fieldId);
+	
+	if (field.val() == '') {
+		highlightField(field, false);
+		throw "Failed to validate field: " + field; 
+	}  
+}
+  
+function softValidateField(fieldId, acceptsOnlyNumbers, acceptsLetters, acceptsSpaces) {
+	try {
+		validateField(fieldId, acceptsOnlyNumbers, acceptsLetters, acceptsSpaces);
+	} catch (err) {
+		console.log(err); 
+		highlightField($('#' + fieldId), false);
+		return;   
+	}  
+	   
+	highlightField($('#' + fieldId), true);
+}
+
+
+function taskFormValidator() {} 
